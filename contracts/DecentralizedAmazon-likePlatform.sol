d42dea223b8f200ef480cbaddf9731abbede5efe
zara shahid <shzara080@gmail.com>
2025-09-25 23:43:38 +0530

    Update DecentralizedAmazon-likePlatform.sol

--- contracts/DecentralizedAmazon-likePlatform.sol
+++ contracts/DecentralizedAmazon-likePlatform.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: mit
+// SPDX-License-Identifier:
 pragma solidity ^0.8.19;
 
 contract DecentralizedAmazonPlatform {
@@ -275,6 +275,7 @@ contract DecentralizedAmazonPlatform {
 
     receive() external payable {}
 }
+
 
 
 
